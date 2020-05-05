75f14f8f39167f5ec013da3a9eac3bd76928bd33
zina <2316650427@qq.com>
2020-05-05 19:29:58 +0800

    slider demo

---
+++ chapter1/webpack-basic/src/a.js
@@ -0,0 +1,9 @@
+// CommonJs
+// module.exports={
+//   name:'haha'
+// }
+
+//es6规范
+export default {
+    name: 'hahah'
+}
--- chapter1/webpack-basic/src/index.js
+++ chapter1/webpack-basic/src/index.js
@@ -1,1 +1,5 @@
-console.log('hahahah');
+// let a = require('./a.js)
+console.log('hahahahfff');
+import a from './a.js'
+console.log(a);
+
